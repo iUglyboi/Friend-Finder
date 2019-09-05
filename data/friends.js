0c14b8b338ca16e3e54799391091cced5d30832a
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-05 15:28:44 -0400

    friends.js finished

---
+++ data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+    name: "Beyonce",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiMhaD2s7rkAhUCXqwKHWdsC0kQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.usatoday.com%2Fstory%2Flife%2Fentertainthis%2F2018%2F07%2F26%2Fbeyonce-shares-adorable-rare-photos-twins-rumi-and-sir%2F840202002%2F&psig=AOvVaw01iUjmFrSSO769n_EdtpoX&ust=1567797755554841",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "3",
+        "1",
+        "5",
+        "1",
+        "4",
+        "3",
+        "1"
+    ]
+},
+{
+    name: "Tupac",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiN4NHXtLrkAhUSDKwKHQCqCVYQjRx6BAgBEAQ&url=http%3A%2F%2Fgoogle.com%2Fsearch%3Ftbm%3Disch%26q%3DTupac%2BShakur&psig=AOvVaw2w0HwqwHzWA5GOrT5iVVVj&ust=1567797961842515",
+    scores: [
+        "3",
+        "1",
+        "5",
+        "4",
+        "1",
+        "5",
+        "1",
+        "1",
+        "5",
+        "3"
+    ]
+},
+{
+    name: "Biggie Smalls",
+    photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.complex.com%2Fcomplex%2Fimages%2Fc_limit%2Cdpr_auto%2Cq_90%2Cw_720%2Ffl_lossy%2Cpg_1%2Fwsqfoft4mwwjk71qmvic%2Fbiggie&imgrefurl=https%3A%2F%2Fwww.complex.com%2Fmusic%2F2017%2F03%2Fhow-hip-hop-would-be-different-if-the-notorious-big-were-alive&docid=HXQrVfYHXkOfZM&tbnid=P9Jc7p0JexOEJM%3A&vet=10ahUKEwivp8HntLrkAhVNb60KHRuXAH4QMwh8KAEwAQ..i&w=680&h=440&bih=744&biw=1440&q=Biggie&ved=0ahUKEwivp8HntLrkAhVNb60KHRuXAH4QMwh8KAEwAQ&iact=mrc&uact=8",
+    score: [
+        "3",
+        "1",
+        "4",
+        "1",
+        "4",
+        "3",
+        "5",
+        "1",
+        "5",
+        "4"		
+    ]
+}
+];
+
+module.exports = friends;
